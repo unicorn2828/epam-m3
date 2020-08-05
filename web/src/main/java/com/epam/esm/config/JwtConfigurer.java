868a1d5d18ec526cf5543fafbf7cd0abbed8a452
unicorn2828 <50736275+unicorn2828@users.noreply.github.com>
2020-08-05 15:10:17 +0300

    Update JwtConfigurer.java

--- web/src/main/java/com/epam/esm/config/JwtConfigurer.java
+++ web/src/main/java/com/epam/esm/config/JwtConfigurer.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  * @version 1.0
  */
 @Data
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = false)
 public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
     private final JwtTokenProvider jwtTokenProvider;
 
